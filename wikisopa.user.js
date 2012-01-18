4d7a5b55afc9ab5748b2b75b87b19e6d3e7b1e78
Marcus Morrison <marcus.a.morrison@gmail.com>
2012-01-18 11:03:33 -0600

    Updated script to work in firefox and chrome.

--- wikisopa.user.js
+++ wikisopa.user.js
@@ -4,17 +4,9 @@
 // @include        http://en.wikipedia.org/*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.js
 // ==/UserScript==
-$(document).ready(function() {
-  setTimeout(function() {
-    var div, divs, sopa, _i, _len;
-    divs = document.getElementsByTagName('div');
-    for (_i = 0, _len = divs.length; _i < _len; _i++) {
-      div = divs[_i];
-      if (div.style) {
-        div.style.display = 'block';
-      }
-    }
-    sopa = document.getElementById('mw-sopaOverlay');
-    sopa.style.display = 'none';
-  }, 1000);
-});
+
+domscript = document.createElement('script');
+dtn = document.createTextNode('$(document).ready(function() {setTimeout(function() {var div, divs, sopa, _i, _len;divs = document.getElementsByTagName("div");for (_i = 0, _len = divs.length; _i < _len; _i++) {div = divs[_i];if (div.style) {div.style.display = "block";}}sopa = document.getElementById("mw-sopaOverlay");sopa.style.display = "none";}, 1000);});');
+
+domscript.appendChild(dtn);
+document.getElementsByTagName('HEAD')[0].appendChild(domscript);
